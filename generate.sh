b4b5278a97b02ce12b2846730c21f5f6324e3eab
MarijnS95 <marijns95@gmail.com>
2019-10-08 12:43:16 +0200

    Move folder one directory up.
    Expecting kernel/mainline/ instead of kernel/sony/mainline

--- generate.sh
+++ generate.sh
@@ -1,15 +1,21 @@
 set -e
 
-android_root="../../../../"
+if [ -z "$ANDROID_ROOT" ]; then
+    ANDROID_ROOT="`realpath ../../../`"
+    echo "WARNING: Guessing Android root at: $ANDROID_ROOT"
+fi
+
 # Name restriction in the android build system:
 _header_dir=kernel-headers
 _kernel_dir=`realpath ../kernel`
-_kernel_out="$android_root/out/mainline-kernel"
+_kernel_out="$ANDROID_ROOT/out/mainline-kernel"
 
 echo "Generating kernel headers from $_kernel_dir to $_header_dir"
 
 pushd $_kernel_dir
-make ARCH=arm64 O=$_kernel_out -j$(nproc) defconfig headers_install
+    make ARCH=arm64 O=$_kernel_out -j$(nproc) defconfig headers_install
 popd
 
 cp -r $_kernel_out/usr/include $_header_dir
+
+# TODO: Run bionic header sanitizer?
